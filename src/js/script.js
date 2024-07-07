eaeb34bc080905cd9792b150c3adaa66c0d11bd8
peije21 <fabiotwist1@gmail.com>
2024-07-07 16:25:58 -0300

    ultimo add hoje

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,17 @@
 const tarefas = localStorage.length >= 1 ? JSON.parse(localStorage.getItem("tarefas")) : []
-
 tarefas.forEach(el => {
     mostraTarefas(el)
 });
 
+
 const subForm = document.querySelector("#subForm")
 subForm.addEventListener("click", (e) => {
     e.preventDefault()
-    coletaTarefas()
+    validaTarefas()
 })
 
 
-function coletaTarefas(){
+function validaTarefas(){
     let tarefaAfazer = document.querySelector("#tarefaAfazer").value
     
     if(tarefaAfazer == " " || tarefaAfazer == ""){
@@ -38,11 +38,23 @@ function mostraTarefas({tarefaAfazer, categoriaDaTarefa, idTarefa}){
     const itemTarefa = document.createElement("li")
     itemTarefa.setAttribute("id", idTarefa)
     itemTarefa.setAttribute("class", categoriaDaTarefa)
-    itemTarefa.innerHTML = `${tarefaAfazer}`
+    itemTarefa.innerHTML = `${tarefaAfazer}
+    <svg class="excluiTarefa" id="${idTarefa}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>`
     ul.append(itemTarefa)
-
     concluiTarefa(idTarefa)
+    excluirTarefa(idTarefa)
 }
+function excluirTarefa(idTarefa){
+
+    const btnTarefa = Array.from(document.querySelectorAll(".excluiTarefa"))
+    btnTarefa.forEach(el => {
+        el.addEventListener("click", () => {
+            btnTarefa.id == idTarefa ? console.log("sim", idTarefa): console.log("não", idTarefa)
+        })
+    })
+}
+
+
 
 function concluiTarefa(idTarefa){
     const newIdTarefa = "#" + idTarefa
