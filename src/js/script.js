1637b2c0cd841a5fabda1abcecf90895692df296
peije21 <fabiotwist1@gmail.com>
2024-07-17 23:03:03 -0300

    falta scripts para edit no modal da tarefa

--- src/js/script.js
+++ src/js/script.js
@@ -1,64 +1,85 @@
 // Inicializa as tarefas a partir do localStorage
-let tarefas = localStorage.length >= 1 ? JSON.parse(localStorage.getItem("tarefas")) : []
-tarefas.forEach(el => {
-    mostraTarefas(el)
+let tarefas =
+  localStorage.length >= 1 ? JSON.parse(localStorage.getItem("tarefas")) : [];
+tarefas.forEach((el) => {
+  mostraTarefas(el);
 });
 
-const subForm = document.querySelector("#subForm")
+const subForm = document.querySelector("#subForm");
 subForm.addEventListener("click", (e) => {
-    e.preventDefault()
-    validaTarefas()
-})
+  e.preventDefault();
+  validaTarefas();
+});
 
-function validaTarefas(){
-    let tarefaAfazer = document.querySelector("#tarefaAfazer").value
-    
-    if(tarefaAfazer.trim() === ""){
-        tarefaAfazer = "Nova tarefa"
-    }
+function validaTarefas() {
+  let tarefaAfazer = document.querySelector("#tarefaAfazer").value;
 
-    const idTarefa = "abc" + (Math.floor(Math.random() * 10000))
+  if (tarefaAfazer.trim() === "") {
+    tarefaAfazer = "Nova tarefa";
+  }
 
-    const novaTarefa = {
-        tarefaAfazer,
-        idTarefa
-    }
-    
-    tarefas.push(novaTarefa)
-    mostraTarefas(novaTarefa)
-    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+  const idTarefa = "abc" + Math.floor(Math.random() * 10000);
+
+  const novaTarefa = {
+    tarefaAfazer,
+    idTarefa,
+  };
+
+  tarefas.push(novaTarefa);
+  mostraTarefas(novaTarefa);
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
 }
 
-function mostraTarefas({tarefaAfazer, idTarefa}) {
-    const ul = document.querySelector("#afazeresTarefas")
-    const itemTarefa = document.createElement("li")
-    itemTarefa.setAttribute("id", idTarefa)
-    itemTarefa.setAttribute("class", "tarefaAfazer")
+function mostraTarefas({ tarefaAfazer, idTarefa }) {
+  const ul = document.querySelector("#afazeresTarefas");
+  const itemTarefa = document.createElement("li");
+  itemTarefa.setAttribute("id", idTarefa);
+  itemTarefa.setAttribute("class", "tarefaAfazer");
 
-    itemTarefa.innerHTML = `${tarefaAfazer} <i class="fa-solid fa-gear" id="trashBtn"></i>`
-    
-    ul.append(itemTarefa)
-    concluiTarefa(idTarefa)
+  itemTarefa.innerHTML = `${tarefaAfazer} <i class="fa-solid fa-gear" id="configsBtn"></i>`;
+
+  ul.append(itemTarefa);
+  concluiTarefa(idTarefa);
 }
 
 document.addEventListener("click", (e) => {
-    const alvoEl = e.target
-    if(alvoEl.id === "trashBtn"){
-        const parenteEl = alvoEl.closest("li")
-        excluirTarefa(parenteEl)
-    }
-})
+  const alvoEl = e.target;
+  console.log(alvoEl.id);
+  if (alvoEl.id === "configsBtn") {
+    var parenteEl = alvoEl.closest("li");
+    document.querySelectorAll(".modal")[0].classList.remove("hidden");
+    document.querySelectorAll(".itensTarefa")[0].classList.remove("hidden");
+    editTarefa();
+  }
+  if (alvoEl.id === "excluirTarefa") {
+    excluirTarefa(parenteEl);
+  }
 
-function concluiTarefa(idTarefa){
-    const leitorTarefa = document.getElementById(idTarefa)
-    leitorTarefa.addEventListener("click", (e) => {
-        leitorTarefa.classList.toggle("concluida")
-    })   
+  if (alvoEl.id === "confirmarAlterarTarefa") {
+    document.querySelectorAll(".modal")[0].classList.add("hidden");
+    document.querySelectorAll(".itensTarefa")[0].classList.add("hidden");
+  }
+});
+
+function editTarefa() {
+  // definir inputs e descrição p/ alterar
+  // aguardar o click da exclusão ou confirmação []
+  // tirar a classe hidden e incluí-la nas tarefas [x]
+  // pegar o texto
+  // alterar o objeto vindo do localstorage
+  // confirmar a alteração do objeto no localstorage
 }
 
-function excluirTarefa(parenteEl){
-    const idTarefa = parenteEl.id
-    tarefas = tarefas.filter(tarefa => tarefa.idTarefa !== idTarefa)
-    localStorage.setItem("tarefas", JSON.stringify(tarefas))
-    parenteEl.remove()
+function concluiTarefa(idTarefa) {
+  const leitorTarefa = document.getElementById(idTarefa);
+  leitorTarefa.addEventListener("click", (e) => {
+    leitorTarefa.classList.toggle("concluida");
+  });
+}
+
+function excluirTarefa(parenteEl) {
+  const idTarefa = parenteEl.id;
+  tarefas = tarefas.filter((tarefa) => tarefa.idTarefa !== idTarefa);
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  parenteEl.remove();
 }
